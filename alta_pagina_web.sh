8878dec6233462b8739bbf6c1b2e72e214ca7004
FranciscoRomero23 <4afromero@gmail.com>
2019-02-11 12:30:53 +0100

    Script planificado

---
+++ alta_pagina_web.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script bash que permite habilitar una nueva página web y su acceso por ftp.
+# Francisco José Romero Morillo, 2019
+
+## Parametros
+# Clave ssh
+clavessh=""
+# Servidores
+servidorweb=""
+servidordns=""
+
+# Funciones
+
+function Crea_Virtualhost {
+
+}
+function Crea_Directorio {
+}
+function Habilita_Pagina {
+}
+
+

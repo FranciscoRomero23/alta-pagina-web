29c0b9464bd8beed2b8a30b0202e3e50a47fbe77
FranciscoRomero23 <4afromero@gmail.com>
2019-02-11 13:28:18 +0100

    Segunda funcion creada

--- alta_pagina_web.sh
+++ alta_pagina_web.sh
@@ -21,4 +21,15 @@ function Crea_Virtualhost {
 	# Habilitamos el virtualhost
 	ssh -i $clavessh root@$servidorweb ln -s /etc/httpd/sites-available/$nombre_pagina.conf /etc/httpd/sites-enabled/$nombre_pagina.conf
 }
+function Crea_Directorio {
+	# Creamos el directorio
+	ssh -i $clavessh root@$servidorweb mkdir -p /var/www/$nombre_pagina/public_html
+	# Creamos un index.html de ejemplo
+	ssh -i $clavessh root@$servidorweb touch /var/www/$nombre_pagina/public_html/index.html
+	scp -i $clavessh ./default-index.html root@$servidorweb:/var/www/$nombre_pagina/public_html/index.html
+        ssh -i $clavessh root@$servidorweb sed -i s/'paginaweb'/$nombre_pagina/g /var/www/$nombre_pagina/public_html/index.html
+	# Reiniciamos el servidor web
+	ssh -i $clavessh root@$servidorweb systemctl restart httpd
+}
 Crea_Virtualhost
+Crea_Directorio
